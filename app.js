2f0695c32ec0dabef0e5b883e6ecca8bc2fb7ed5
harim <zing10@naver.com>
2021-09-08 17:03:06 +0900

    test1

---
+++ app.js
@@ -0,0 +1,30 @@
+// const age = 96;
+
+// function calculatKrage(ageOfForeigner){
+//    ageOfForeigner + 2;
+//    console.log(ageOfForeigner);
+//    return "hello"
+// }
+
+// const krAge = calculatKrage(age); 
+// console.log(krAge);
+
+
+// const aa = {
+//    plus: function (a,b){
+//       console.log("안뇽");
+//      return a + b;
+//      //리턴하면 그아래 작업은 안한다
+      
+//    },
+// };
+
+// const dd = aa.plus(2,3);
+
+const age = prompt("how old are you");
+
+console.log(typeof age);// typeof 타입을 알수있다.
+
+//테스트용 텍스느
+
+console.log("안녕")
